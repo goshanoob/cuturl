da167a67e1e3345327e131f7c258120323dc4d65
goshanoob <goshanoob@mail.ru>
2022-01-23 17:21:30 +0500

    Написан README

--- includes/config.php
+++ includes/config.php
@@ -6,9 +6,9 @@ define('HOST', $_SERVER['HTTP_HOST']);
 
 // Настройки подключения к базе данных.
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'cuturl');
-define('DB_USER', 'admin');
-define('DB_PASS', '12345');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
-// Запуск сессии на всех ст
+// Запуск сессии на всех страницах
 session_start();
